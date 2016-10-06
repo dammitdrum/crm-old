5d174d73f2356c1204ce2df51bf3ba6620ad8a7f
dammitdrum <dammitdrum@gmail.com>
2016-10-06 10:32:29 +0300

    06.10.16

--- server.js
+++ server.js
@@ -5,16 +5,20 @@ var express = require('express'),
     mongoose = require('mongoose');
 
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/front'));
+//app.use(express.static(__dirname + '/front'));
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
 
-mongoose.Promise = global.Promise;
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+/*mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://dbuser:password@ds035766.mlab.com:35766/crmdb');
 var db = mongoose.connection;
 db.on('error', function (err) {
@@ -23,7 +27,7 @@ db.on('error', function (err) {
 db.once('open', function callback () {
     console.log("Connected to DB!");
     //app.listen(1337);
-    
+
     console.log('Server is running');
 });
 
@@ -260,6 +264,6 @@ app.delete('/partners/delete/:id', function (req, res){
             }
         });
     });
-});
+});*/
 
 
